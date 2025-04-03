9440900d9ceadd06f7c39c96a95683c1672bc591
Théo Dufrene <43671109+itsme-to@users.noreply.github.com>
2025-04-03 16:29:33 +0200

    New anyOf, atLeastOf & isExpressionTrue condtions

---
+++ src/core/services/conditions/impl/anyOf.ts
@@ -0,0 +1,15 @@
+import { Condition, ConditionData, Context, Variable } from '@itsmybot';
+
+export default class AnyOfCondition extends Condition {
+  id = "anyOf";
+
+  async isMet(condition: ConditionData, context: Context, variables: Variable[]) {
+    const conditionsConfig = condition.args.getSubsectionsOrNull("conditions");
+    if (!conditionsConfig) return condition.missingArg("conditions");
+
+    const conditions = this.manager.services.condition.buildConditions(conditionsConfig, false);
+
+    const isMet = await Promise.all(conditions.map(condition => this.manager.services.condition.isConditionMet(condition, context, variables)))
+    return isMet.some(result => result);
+  }
+}
---
+++ src/core/services/conditions/impl/isExpressionTrue.ts
@@ -0,0 +1,27 @@
+import { Condition, ConditionData, Context, Variable } from '@itsmybot';
+import Utils from '@utils';
+import { Parser } from 'expr-eval';
+
+export default class IsExpressionTrueCondition extends Condition {
+  id = "isExpressionTrue";
+
+  async isMet(condition: ConditionData, context: Context, variables: Variable[]) {
+    const expressionArg = condition.args.getStringOrNull("value");
+    if (!expressionArg) return condition.missingArg("value");
+
+    const expression = await Utils.applyVariables(expressionArg, variables, context);
+    if (!expression) return condition.logError("Invalid expression");
+    try {
+      const parsedExpression = Parser.parse(expression);
+      const result = parsedExpression.evaluate();
+
+      if (typeof result !== 'boolean') {
+        return condition.logError("Expression did not evaluate to a boolean");
+      }
+
+      return result;
+    } catch (error) {
+      return condition.logError("Error evaluating expression: " + error);
+    }
+  }
+}
