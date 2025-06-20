c42332990ed86c2ac48dc4f980553208f991940f
Théo Dufrene <43671109+itsme-to@users.noreply.github.com>
2025-06-20 10:52:26 +0200

    Renamed aboveMembers & bellowMembers conditions to memberCountAbove & memberCountBelow

--- src/core/services/conditions/impl/memberCountAbove.ts
+++ src/core/services/conditions/impl/memberCountAbove.ts
@@ -1,13 +1,13 @@
 import { Condition, ConditionData, Context, Variable } from '@itsmybot';
 
-export default class BellowMembersCondition extends Condition {
-  id = "bellowMembers";
+export default class MemberCountAboveCondition extends Condition {
+  id = "memberCountAbove";
 
   isMet(condition: ConditionData, context: Context, variables: Variable[]) {
     if (!context.guild) return condition.missingContext("guild");
     const amount = condition.args.getNumberOrNull("amount");
     if (!amount) return condition.missingArg("amount");
 
-    return context.guild!.memberCount < amount;
+    return context.guild.memberCount > amount;
   }
 }
