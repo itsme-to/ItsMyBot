6dd16ed634d331cd5f448695c0a6dd8a3b90b07d
Théo Dufrene <43671109+itsme-to@users.noreply.github.com>
2026-02-06 18:58:30 +0100

    buttonClick trigger now add args, allowing you to transfert information with button custom id

--- src/core/services/events/impl/buttonClick.ts
+++ src/core/services/events/impl/buttonClick.ts
@@ -7,17 +7,36 @@ export default class ButtonClickEvent extends Event {
   async execute(interaction: ButtonInteraction<'cached'>, user: User) {
     if (!interaction.customId.startsWith("script_")) return;
 
+    const args: string[] = [];
+    let customId = interaction.customId;
+
+    if (interaction.customId.includes(':')) {
+      const split = interaction.customId.match(/('.*?'|".*?"|[^:]+)+/g);
+      if (split) {
+        customId = split[0];
+        args.push(...split.slice(1).map(arg => arg.replace(/^['"]|['"]$/g, '')));
+      }
+    }
+
     const context: Context = {
       guild: interaction.guild || undefined,
       member: interaction.member || undefined,
       user: user,
       channel: interaction.channel || undefined,
-      content: interaction.customId,
+      content: customId,
       interaction: interaction,
       message: interaction.message,
     };
 
-    const variables: Variable[] = [{ name: 'button_custom_id', value: interaction.customId }];
+    const variables: Variable[] = [
+      { name: 'button_custom_id', value: customId },
+      { name: 'button_args_count', value: args.length },
+      { name: 'button_args', value: args.join(', ') }
+    ];
+
+    for (let i = 0; i < args.length; i++) {
+      variables.push({ name: `button_arg_${i}`, value: args[i] });
+    }
 
     this.manager.services.engine.event.emit('buttonClick', context, variables);
   }
