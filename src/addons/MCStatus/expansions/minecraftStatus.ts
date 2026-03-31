53fdf18c812239354d2deaf274a343065cbab216
Théo Dufrene <43671109+itsme-to@users.noreply.github.com>
2026-03-31 11:16:57 +0200

    MCStatus: removed configuration

--- src/addons/MCStatus/expansions/minecraftStatus.ts
+++ src/addons/MCStatus/expansions/minecraftStatus.ts
@@ -7,34 +7,27 @@ export default class MCStatusExpansion extends Expansion<MCStatsAddon> {
   status: Map<string, ServerStats> = new Map();
 
   async onRequest(context: Context, placeholder: string) {
-    const servers = this.addon.configs.config.getSubsections("servers");
-    const serverId = placeholder.split("_")[0];
-
-    const server = servers.find((server) => server.getString('id') === serverId);
-    if (!server) return "Server not found";
+    const serverIp = placeholder.split("_")[0];
 
     let stats: ServerStats | undefined = undefined;
-    if (this.status.has(serverId)) {
-      if (this.cooldown.isOnCooldown(serverId)) {
-        stats = this.status.get(serverId);
+    if (this.status.has(serverIp)) {
+      if (this.cooldown.isOnCooldown(serverIp)) {
+        stats = this.status.get(serverIp);
       }
     }
 
     if (!stats) {
-      stats = await this.addon.fetchStatus(server.getString("address"), server.getNumber("port"));
-      if (stats) {
-        this.status.set(serverId, stats);
-        this.cooldown.setCooldown(serverId);
-        return this.handlePlaceholder(placeholder, serverId, stats);
-      }
-      return "Server not found";
+      stats = await this.addon.fetchStatus(serverIp);
+      this.status.set(serverIp, stats);
+      this.cooldown.setCooldown(serverIp);
+      return this.handlePlaceholder(placeholder, serverIp, stats);
     } else {
-      return this.handlePlaceholder(placeholder, serverId, stats);
+      return this.handlePlaceholder(placeholder, serverIp, stats);
     } 
   }
 
-  async handlePlaceholder(placeholder: string, serverId: string, stats: ServerStats) {
-    switch (placeholder.substring(serverId.length + 1)) {
+  handlePlaceholder(placeholder: string, serverIp: string, stats: ServerStats) {
+    switch (placeholder.substring(serverIp.length + 1)) {
       case "players":
         return stats.players ? `${stats.players.online}` : "0";
       case "max_players":
@@ -51,6 +44,8 @@ export default class MCStatusExpansion extends Expansion<MCStatsAddon> {
         return stats.online ? 'true' : 'false';
       case "status":
         return stats.online ? this.addon.lang.getString("online") : this.addon.lang.getString("offline");
+      case "icon":
+        return `https://mcstatus.snowdev.com.br/api/favicon/${stats.host}:${stats.port}/favicon.png`
       default:
         return "Invalid placeholder";
     }
