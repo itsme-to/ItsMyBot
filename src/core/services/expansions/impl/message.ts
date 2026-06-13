79441b940ec85fec66033a185c085dafdfba031c
Théo Dufrene <43671109+itsme-to@users.noreply.github.com>
2026-06-13 12:00:01 +0200

    New message placeholder

--- src/core/services/expansions/impl/message.ts
+++ src/core/services/expansions/impl/message.ts
@@ -12,10 +12,14 @@ export default class MessageExpansion extends Expansion {
         return context.message.id;
       case 'content':
         return Utils.blockPlaceholders(context.message.content);
+      case 'is_content_empty':
+        return context.message.content.length === 0 ? 'true' : 'false';
       case 'author_id':
         return context.message.author.id;
       case 'url':
         return context.message.url;
+      case 'attachment_count':
+        return context.message.attachments.size.toString();
     }
   }
 }
